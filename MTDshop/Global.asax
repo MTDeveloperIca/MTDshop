501ec9cc86295de9ed729de88231e85fe7b765af
kattya <kattya.garcia@outlook.es>
2016-07-19 00:25:23 -0500

    Crear proyecto

---
+++ MTDshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MTDshop.MvcApplication" Language="C#" %>
---
+++ MTDshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MTDshop.Startup))]
+namespace MTDshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
